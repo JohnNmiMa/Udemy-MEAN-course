974cf82b2baa3c531a2ff71c08705c727ba11f0e
John Marks <john@gitlab.newtonon.com>
2020-03-13 15:39:16 -0600

    Added node.js backend server, doing /api/posts GET and POST.

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); // Start the express app
 
-// An express app is just a big chain of middlewears that we apply to the incoming requests.
-// "use" a new middlewear
 app.use((req, res, next) => {
-    console.log('First middleware');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.send('Hello from express'); // will explicity end() the response
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    res.status(201).json({
+        message: 'Post added sucessfully'
+    })
+});
+
+app.get('/api/posts', (req, res, next) => {
+    const posts = [
+        { id: 'fkdlaljgjglaj', title: 'First server-side post', content: 'This is coming form the server'},
+        { id: 'fkdlaljgjglak', title: 'Second server-side post', content: 'This is coming form the server'}
+    ];
+    res.status(200).json({
+        message: 'Posts fetched succesfully',
+        posts: posts
+    })
 });
 
 module.exports = app;
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -17,7 +17,7 @@ export class PostListComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
-        this.posts = this.postsService.getPosts();
+        this.postsService.getPosts();
         this.postSub =  this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
             this.posts = posts;
         })

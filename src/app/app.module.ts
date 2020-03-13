69b8077621d59fb5c18b5fde6bff7fde35ba871a
John Marks <john@gitlab.newtonon.com>
2020-03-13 12:42:30 -0600

    Added Subject observable in a posts-server to communicate posts, thereby removing the property and event bindings.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
